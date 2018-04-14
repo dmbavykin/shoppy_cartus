35ef7ef292e402bb8e59e11da7e6c60732faa84d
Dmitriy Bavykin <1996@3g.ua>
2018-04-14 15:06:28 +0300

    fix abilities
    	modified:   Gemfile.lock
    	modified:   app/controllers/shoppy_cartus/orders_controller.rb
    	modified:   app/models/shoppy_cartus/ability.rb
    	modified:   lib/shoppy_cartus/version.rb
    	deleted:    test/integration/navigation_test.rb
    	deleted:    test/shoppy_cartus_test.rb
    	deleted:    test/test_helper.rb

--- app/controllers/shoppy_cartus/orders_controller.rb
+++ app/controllers/shoppy_cartus/orders_controller.rb
@@ -1,23 +1,28 @@
 module ShoppyCartus
   class OrdersController < ApplicationController
+    before_action "authenticate_#{ShoppyCartus.user_class.downcase}!".to_sym
+    # authorize_resource class: 'ShoppyCartus::Order'
 
     def index
+      authorize! :index, ShoppyCartus::Order
       @orders = current_user.orders.executed
       @orders = @orders.by_state(params[:state].to_sym) if valid_state?
     end
 
     def show
       @_order = ShoppyCartus::Order.find_by(id: params[:id]).decorate
+      authorize! :show, @_order.object
     end
 
     def confirm
       order = ShoppyCartus::Order.find_by(id: params[:order_id])
       if order.confirmation_token == params[:token]
+        authorize! :confirm, order
         order.process
         order.save
-        return redirect_to main_app.root_path, notice: t('order.confirmed')
+        return redirect_to order_items_path, notice: t('order.confirmed')
       end
-      redirect_to main_app.root_path, alert: t('order.wrong_token')
+      redirect_to order_items_path, alert: t('order.wrong_token')
     end
 
     private
--- app/models/shoppy_cartus/ability.rb
+++ app/models/shoppy_cartus/ability.rb
@@ -9,7 +9,7 @@ module ShoppyCartus
 
       can :manage, OrderItem, order_id: order.id
       can :manage, Coupon, order_id: order.id
-      can :manage, Order, user_id: user.id
+      can %i[index show confirm], Order, user_id: user.id
       can %i[create update], CreditCard, user_id: user.id
     end
   end
